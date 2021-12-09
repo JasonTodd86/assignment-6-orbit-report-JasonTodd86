e949e9bf92e3f306e31ab2a0bfeee81303f95312
JasonTodd86 <j.chris.todd@gmail.com>
2021-12-09 13:37:13 -0600

    Finished

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
 @Component({
-  selector: 'app-orbit-list',
-  templateUrl: './orbit-list.component.html',
-  styleUrls: ['./orbit-list.component.css']
+	selector: 'app-orbit-list',
+	templateUrl: './orbit-list.component.html',
+	styleUrls: ['./orbit-list.component.css']
 })
 
 export class OrbitListComponent implements OnInit {
 
-  @Input() satellites: Satellite[];
-  constructor() { }
+	displayList = [];
 
-  ngOnInit() {
-  }
+	@Input() satellites: Satellite[];
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 	sort(column: string): void {
 		// array.sort modifies the array, sorting the items based on the given compare function
